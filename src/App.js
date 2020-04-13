83f638cabcdaa6ef9c38616351e6f5e4e0cecac6
Ken Jannette <kennethjannette@kj.local>
2020-04-13 18:52:26 -0400

    Fixed issues after second meltdown of the day

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
 import React from 'react';
-import { withStyles } from '@material-ui/core';
 import MainPage from './components/MainPage.js'
 import SecondPage from './components/SecondPage.js'
-import MainPageStyles from './Styles/MainPageStyles';
-import SecondPageStyles from './Styles/SecondPageStyles';
-import AppRouter from './routers/AppRouter.js'
+import Grid from '@material-ui/core/Grid';
+import { Router, Route, Switch, Link, NavLink } from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
+import {createMuiTheme, makeStyles, ThemeProvider} from '@material-ui/core/styles';
 
+
+const theme = createMuiTheme({
+ 
+  });
+  
+  const useStyles = makeStyles(theme => ({
+    root: {
+      flexGrow: 1,
+    },
+
+  }));
+
+export const history = createHistory();
+
+
+export default function App() {
+    const classes = useStyles();
+    return (
+  
+      <ThemeProvider theme={theme}>
+        <div className={classes.root} >
+          <Grid container >
+            <Router history={history}>
+              <Route exact path="/" component={MainPage}/>
+              <Route exact path="/secondpage" component={SecondPage}/>
+            </Router>
+          </Grid>
+        </div>
+      </ThemeProvider>
+    );
+  }
+
+
+
+
+/*
 class App extends React.Component {
     
     render() {
@@ -19,3 +55,4 @@ class App extends React.Component {
 }
    
 export default withStyles(MainPageStyles)(App);
+*/
