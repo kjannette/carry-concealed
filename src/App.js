033921721a6518b6a6b01abc00df3dd3d6a9faf1
Ken Jannette <kennethjannette@kj.local>
2020-04-19 14:27:05 -0400

    Router now working and code/app structre cleaned up, some work still to be done

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
 import React from 'react';
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
-import MainPage from './components/MainPage.js'
-import OpenCarryPage from './components/OpenCarryPage.js'
-import SecondPage from './components/SecondPage.js'
-import { Router, Route, Switch } from 'react-router-dom';
-import createHistory from 'history/createBrowserHistory';
 import {createMuiTheme, makeStyles, responsiveFontSizes } from '@material-ui/core/styles';
+import AppRouter from './routers/AppRouter.js'
 
 let theme = createMuiTheme();
 theme = responsiveFontSizes(theme);
 
-  let useStyles = makeStyles(theme => ({
+let useStyles = makeStyles(theme => ({
 
-  }));
-
-  export function ScrollToTop() {
-    const { pathname } = useLocation();
-  
-    useEffect(() => {
-      window.scrollTo(0, 0);
-    }, [pathname]);
-  
-    return null;
-  }
-
-  export const history = createHistory();
+}));
 
 export default function App() {
   
@@ -34,18 +16,10 @@ export default function App() {
   return (
 
     <div className={classes.root} >
-
-      <Router history={history}>
-        <ScrollToTop />
-        <Switch>
-            <Route exact path="/" component={MainPage}/>
-            <Route exact path="/secondpage" component={SecondPage}/>
-            <Route exact path="/opencarry" component={OpenCarryPage}/>
-        </Switch>
-      </Router>
+      
+      <AppRouter />
 
     </div>
    
   );
 }
-
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
 import MainPage from '../components/MainPage.js'
 import OpenCarryPage from '../components/OpenCarryPage.js'
 import SecondPage from '../components/SecondPage.js'
@@ -15,25 +13,19 @@ export const history = createHistory();
 let theme = createMuiTheme();
 theme = responsiveFontSizes(theme);
 
-  let useStyles = makeStyles(theme => ({
+let useStyles = makeStyles(theme => ({
 
-  }));
+}));
 
 class AppRouter extends React.Component {
-
-    constructor() {
-        super();
-    
-      }
 
       render() {
 
         const { classes } = this.props;
 
         return (
-
+            
             <Router history={history}>
-            <ScrollToTop />
             <Switch>
                 <Route exact path="/" component={MainPage}/>
                 <Route exact path="/secondpage" component={SecondPage}/>
