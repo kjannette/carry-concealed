906464633a188be1ae2b4fbea6b0319aaae7007f
Ken Jannette <kennethjannette@kj.local>
2020-05-07 14:38:36 -0400

    More work on Michigan content

--- src/components/states/MichiganPage.js
+++ src/components/states/MichiganPage.js
@@ -24,7 +24,7 @@ class MichiganPage extends React.Component {
                         Concealed Weapons Permits
                     </div>
                     <div className={classes.subHeading}>
-                        Obtaining a concealed weapons permit in Michigan
+                        Obtaining a Michigan concealed weapons permit
                     </div>
 
                     <div className={classes.dividerBox}>
@@ -49,24 +49,27 @@ class MichiganPage extends React.Component {
                                     <p>
                                         A Michigan resident must meet basic requirements to obtain a Michigan concealed weapons permit, for example:
                                         <ol>
+
                                             <li>Be at least 21 years of age.</li><br></br>
                                             <li>Be a citizen of the United States or an alien lawfully admitted into the United States.</li><br></br>
                                             <li>Be a legal resident of Michigan and reside in Michigan for at least six months immediately prior to application.  Under the law, an applicant for a concealed weapons permit is 
                                                 deemed a resident of Michigan if he or she has a Michigan drivers license, is registered to vote in Michigan, or can present 
                                                 <a className={classes.link} href="https://www.michigan.gov/msp/0,4643,7-123-1878_1591_3503_4654-10926--,00.html"> other evidence</a> of residency.</li><br></br>
-                                            <li>Have knowledge and training in the safe use and handling of a pistol by successfully completing an appropriate pistol safety training course or class.</li><br></br>
+                                            <li>Have knowledge and 
+                                            <a className={classes.link} href="https://www.michigan.gov/mcoles/0,4607,7-229--148860--,00.html"> training in the safe use and handling of a pistol</a> by
+                                                successfully completing an appropriate pistol safety training course or class.</li><br></br>
                                             <li>Not be on probation, or subject to certain other court orders, such as for legal incapacity (this can be related to mental health issues), or a 
                                                 <a className={classes.link} href="https://www.michigan.gov/documents/msp/PPO_Brochure_2_176129_7.pdf>"> personal protection order.</a></li><br></br> 
                                             <li>Not be prohibited from possessing, using, transporting, selling, purchasing, carrying, shipping, receiving, or distributing a firearm under any other applicable Michigan law.</li><br></br>
                                             <li>Have never been convicted of a felony in Michigan or elsewhere, and a felony charge against the applicant is not pending in Michigan or elsewhere at the time he or she applies for a CPL.</li><br></br>
                                             <li>Have not been dishonorably discharged from the United States Armed Forces.</li><br></br>
-                                            <li>Have not been convicted of a misdemeanor in the eight years immediately preceding the date of the application for a Michigan concealed weapons permit, or have a pending charge for a misdemeanor at the 
+                                            <li>Have not been convicted of a misdemeanor in three to eight years (depnding on the offense) immediately preceding the date of the application for a Michigan concealed weapons permit, or have a pending charge for a misdemeanor at the 
                                                 time he or she applies for the Michigan concealed weapons permit.</li><br></br>
-                                            <li></li><br></br>
-                                            <li></li><br></br>
-                                            <li></li><br></br>
-                                            <li></li><br></br>
-                                            <li></li><br></br>
+                                            <li>Have not been found guilty but mentally ill of any crime, and has not offered a plea of not guilty of, or been acquitted of, any crime by reason of insanity.</li><br></br>
+                                            <li>Is not currently and has never been subject to an order of involuntary commitment in an inpatient or outpatient setting due to mental illness.</li><br></br>
+                                            <li>Not have a diagnosed severe mental illness, which renders them dangerous to themselves or others,  at the time the Michigan concealed weapons permit application is made.</li><br></br>
+                                            <li>Not be under a court order of legal incapacity in this state or elsewhere.</li><br></br>
+                                            <li>Have a valid state-issued driver’s license or personal identification card.</li><br></br>
 
                                         </ol>
                                     </p>
