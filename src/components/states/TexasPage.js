ea6d92d6331f48ffcde3e17d4a8325489803b397
Ken Jannette <kennethjannette@kj.local>
2020-05-11 14:40:48 -0400

    Finished off basic Texas page

--- src/components/states/TexasPage.js
+++ src/components/states/TexasPage.js
@@ -37,28 +37,43 @@ class TexasPage extends React.Component {
 
                                 <Typography className={classes.textLeft}>
 
-                                    <p> Texas is a <Link to="/shallissue" className={classes.link}>“shall issue” </Link>concealed weapons permit state, and is generally very liberal in issuing Permits
-                                        An application for a Texas concealed wepons permit can be <a className={classes.link} href="https://txapps.texas.gov/txapp/txdps/ltc/"></a>completed online.</p>
+                                    <p> Texas is a <Link to="/shallissue" className={classes.link}>“shall issue” </Link>concealed weapons permit state, and is generally very liberal in issuing concealed weapons permits.
+                                        Technically, there is no distinction between a Texas concealed wepons permit and a wepons permit in general.  A holder of a permit can carry 
+                                        either openly, or concealed.  An application for a permit can be <a className={classes.link} href="https://txapps.texas.gov/txapp/txdps/ltc/">completed online.</a></p>
                                     <p>
                                         Like most shall isse concealed weapons permits states, Texas starts with the presumption that an individual should be entitled to carry a concealed weapon, unless specific exceptions are present.  
-                                        For example, in Texas, a resident may not be permitted to carry a concealed weapon if he or she has been convicted of 
-                                        <a className={classes.link} href="https://www.michigan.gov/msp/0,4643,7-123-1878_1591_3503_4654-10926--,00.html"> certain criminal offenses</a>, 
-                                        both misdemeanors and felonies.
                                     </p>
 
                                     <p>
-                                        A Texas resident must meet the following basic requirements to obtain a Michigan concealed weapons permit:
+                                        A Texas resident must meet the following basic requirements to obtain a Texas concealed weapons permit:
                                         <ol>
 
                                             <li>Be at least 21 years of age.</li><br></br>
-                                            <li>Be a citizen of the United States or an alien lawfully admitted into the United States.</li><br></br>
-                                            <li>Be a legal resident of Michigan and reside in Michigan for at least six months immediately prior to application.  Under the law, an applicant for a concealed weapons permit is 
-                                                deemed a resident of Michigan if he or she has a Michigan drivers license, is registered to vote in Michigan, or can present 
-                                                <a className={classes.link} href="https://www.michigan.gov/msp/0,4643,7-123-1878_1591_3503_4654-10926--,00.html"> other evidence</a> of residency.</li><br></br>
-                                            <li>Have knowledge and 
-                                            <a className={classes.link} href="https://www.michigan.gov/mcoles/0,1607,7-229-41626_42485-148857--,00.html"> training in the safe use and handling of a concealed weapon</a> by
-                                                successfully completing an appropriate pistol safety training course or class.</li><br></br>
 
+                                            <li>Be a legal resident of this state for the six-month period preceding the date of application.</li><br></br>
+
+                                            <li>Pass handgun training by a <a className={classes.link} href="https://www.dps.texas.gov/RSD/LTC/instructors.htm">certified weapons permit instructor</a>, either in person, or a 
+                                                combination of in person and online.</li><br></br>
+
+                                            <li>Has not been convicted or charged with a felony or charged with or <a className={classes.link} href="https://www.dps.texas.gov/rsd/ltc/faqs/index.htm#elig">certain misdemeanors.</a> </li><br></br>
+
+                                            <li>Is not a fugitive.</li><br></br>
+
+                                            <li>Is not a chemically dependent person.</li><br></br>
+
+                                            <li>Is not incapable of exercising sound judgment with respect to the proper use and storage of a handgun.</li><br></br>
+
+                                            <li>Is otherwise fully qualified under applicable federal and state law to purchase a handgun.</li><br></br>
+
+                                            <li>Has not been determined by a court to be delinquent in making a child support payment.</li><br></br>
+
+                                            <li>Has not been legally determined to be delinquent in the payment of taxes or certain other government fees.</li><br></br>
+
+                                            <li>Is not currently restricted under a court protective order or subject to a restraining order affecting the spousal relationship, other than a restraining order solely affecting property interests.</li><br></br>
+
+                                            <li>Has not, in the 10 years preceding the date of application, been adjudicated as having engaged in delinquent conduct violating a penal law of the grade of felony (in other words having been convicted as a juvenile, with a sealed record).</li><br></br>
+
+                                            <li>Has not previously made any material misrepresentation (lied), or failed to disclose any material fact, in an application for a weapons permit.</li><br></br>
 
                                         </ol>
                                     </p>
